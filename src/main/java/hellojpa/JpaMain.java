74f19dcef563326186e18ebef9cceadfd5915cc8
KDH_Notebook <destiny1017@nate.com>
2022-04-22 23:44:17 +0900

    JPA 동일성 보장 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,6 +17,27 @@ public class JpaMain {
 
         try {
 
+            Member member1 = new Member();
+            member1.setUserName("member1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUserName("member2");
+            em.persist(member2);
+
+            em.flush();
+
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.find(Member.class, member1.getId());
+            System.out.println("(m1 == m2) = " + (m1 == m2));
+            m1.setUserName("member1.5");
+            System.out.println("(m1 == m2) = " + (m1 == m2));
+            
+            em.flush();
+            
+            Member m3 = em.find(Member.class, member1.getId());
+            System.out.println("(m2 == m3) = " + (m2 == m3));
+
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -21,4 +21,36 @@ public class Member {
 
     @Embedded
     private Address homeAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
